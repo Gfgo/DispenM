cd01b383fa7a111803d9b6e21187815e76ae8d74
Gfgo <gfgo033@hotmail.com>
2021-11-16 13:39:57 -0500

    Inclusion de lcd no funca

--- DispenM/DispenM.ino
+++ DispenM/DispenM.ino
@@ -1,11 +1,12 @@
- #include <Adafruit_NeoPixel.h>
+#include <LiquidCrystal.h>
+#include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
-#define PIN       5                              //Pin control led
-#define NUMpixel 12                              //Pixeles de la tira
+#define PIN      5                              //Pin control led
+#define NUMpixel 8                              //Pixeles de la tira
 
-Adafruit_NeoPixel pixel = Adafruit_NeoPixel(NUMpixel, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel indlleno = Adafruit_NeoPixel(NUMpixel, PIN, NEO_GRB + NEO_KHZ800);
 #define DELAYVAL 500                                  
 
 #include "BluetoothSerial.h"                      //Envio por BLE de Solicitud de apertura 
@@ -14,6 +15,8 @@ Adafruit_NeoPixel pixel = Adafruit_NeoPixel(NUMpixel, PIN, NEO_GRB + NEO_KHZ800)
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
+
+LiquidCrystal lcd(34,35,32,33,25,26);        //lcd
 
 BluetoothSerial SerialBT;
 Servo servo;                                 //Objeto servo
@@ -25,101 +28,84 @@ String datoSerEve= "";
 bool findatoSerEve= false;                  //Si el string esta completo (SerEve)
 
 int pos=0;                                   // variable de posición
+int peso=0;                                  // variable de peso
 
-byte buser=18;                               //control buzzer 
+byte nump=0;                                 //barra de luz
+
 
 
 void setup() {
   SerialBT.begin(115200);                   // inicio bluetooth
-  SerialBT.begin("ControlAcceso");          // Nombre dispositivo
+  Serial.begin(115200);
+  SerialBT.begin("DispenM");          // Nombre dispositivo
   SerialBT.println("Conexion disponible");
+  Serial.println("Conexion disponible");
   datoSerEve.reserve(200);                      //Guardo 200 bytes para datos de llegada
   servo.attach(15);                             //Asigna pin 15 al objeto servo
-  servo2.attach(4);                             //Asigna pin 2 al objeto servo2
-  pixel.begin();                                //Inicia leds
-  pixel.setBrightness(20);
-  pixel.show();
-  pinMode(buser, OUTPUT);  
+  servo2.attach(4);                             //Asigna pin 4 al objeto servo2
+  indlleno.begin();                                //Inicia leds
+  indlleno.setBrightness(20);
+  indlleno.show();
+   
 }
 void loop() {
-  SerialBT.println("Seleccione opcion (1) Abrir o (2) Cerrar");      //Pregunta por opcion
+  SerialBT.println("Seleccione opcion (1) Abrir o (2) Cerrar contenedor");      //Pregunta por opcion
   serialEvent();
   opc=datoSerEve.toInt();
   Serial.println(opc);
-
-  if(opc==0){
-  if((opc==0)&&((servo.read())==180)){
-    for(uint16_t i=0; i<pixel.numPixels(); i++) {
-        pixel.setPixelColor(i, 0, 255,  0);//verde
-        pixel.show();
-      }
-    }
-    else {
-  for(uint16_t i=0; i<pixel.numPixels(); i++) {
-            pixel.setPixelColor(i, 255, 0,  0);//Rojo
-            pixel.show();
-        }
-  }
   
+  peso=analogRead(A0);                        //leo peso en A0
+  nump=map(peso, 0, 61, 0, 8);
 
+//-------------------------------------------------------------------luz llenado de tanque
+  indlleno.clear();
+  for(int i=0; (i<nump || i<nump2); i++) {
+    if (i<=1) rojo=0;
+    if ((i>=2)&&(i<=4)) rojo=128;
+    if (i>=4) rojo=255;
+    if (i<=4) verde=255;
+    if (i==5) verde=128;
+    if (i>=6) verde=0;
+    if (i<=7) azul=0;
+    indlleno.setPixelColor(i, rojo, verde,  azul);//rojo,verde,azul 0-255
+    indlleno.show();
+    delay(PAUSA);
+  }
+//-------------------------------------------------------------------luz llenado de tanque
+  
   switch (opc) {                                //seleccion de caso segun usuario
       case 1:
 //-------------------------------------------------------------------------AQUI ------>>>>>Abro puerta<<<-------
-        SerialBT.println("Abriendo Puerta" );
-        for(uint16_t i=0; i<pixel.numPixels(); i++) {
-            pixel.setPixelColor(i, 255, 255,  0);//amarillo
-            pixel.show();
-        }
+        SerialBT.println("Abriendo Contenedor" );
         for (pos=0; pos<=180; pos++) { // va de 0 a 180 grados
                                               // en pasos de 1 grado
           servo.write(pos);                   // servo va a posición pos
-          digitalWrite(buser, HIGH);                //Alarma
+          servo2.write(pos);
           delay(15);                          // espera 15ms para ir a la posición
         }
-        for (pos=180; pos>=0; pos--) { // va de 180 a 0 grados
-          servo2.write(pos);                   // servo va a posición pos
-          //delay(15);                          // espera 15ms para ir a la posición
-        }
-      if ((servo.read())==180){
-        for(uint16_t i=0; i<pixel.numPixels(); i++) {
-            pixel.setPixelColor(i, 0, 255,  0);//verde
-            pixel.show();
-            delay(500);
-        pixel.setPixelColor(i, 0, 255,  0);//verde
-        pixel.show();
-        }
-      }
-  
-        digitalWrite(buser, LOW);        
+              
 //-----------------------------------------------------------------------------          
           opc=0;                //limpiar el dato
           datoSerEve="";          
           findatoSerEve= false;
-          SerialBT.println("Puerta Abierta");
+          SerialBT.println("Contenedor Abierto");
           break;
       case 2:                             
 //------------------------------------------------------------------AQUI ------>>>>>Cierro puerta<<<-------
-        SerialBT.println("Cerrando Puerta");
-        for(uint16_t i=0; i<pixel.numPixels(); i++) {
-            pixel.setPixelColor(i, 255, 255,  0);//amarillo
-            pixel.show();
-        }
+        SerialBT.println("Cerrando Contenedor");
         for (pos=180; pos>=0; pos--) { // va de 180 a 0 grados
                                               // en pasos de 1 grado
           servo.write(pos);                   // servo va a posición pos
+          servo2.write(pos);
           digitalWrite(buser, HIGH);                //Alarma
           delay(15);                          // espera 15ms para ir a la posición
         }
-        for (pos=0; pos<=180; pos++) { // va de 0 a 180 grados
-          servo2.write(pos);                   // servo va a posición pos
-          //delay(15);                          // espera 15ms para ir a la posición
-        } 
-        digitalWrite(buser, LOW);      
+            
 //-----------------------------------------------------------------------------
         opc=0;                  //limpiar el dato
         datoSerEve="";          
         findatoSerEve= false;
-       SerialBT.println("Puerta Cerrada");
+       SerialBT.println("Contenedor Cerrada");
         break;
       default:
         Serial.println("Seleccion no valida");
